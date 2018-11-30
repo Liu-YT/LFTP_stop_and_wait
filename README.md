006c73c45ee048630307dd1596c7bb01f3fd464b
Liu-YT <1039868176@qq.com>
2018-11-30 13:20:24 +0800

    fix little bug

--- README.md
+++ README.md
@@ -18,6 +18,6 @@
         ```
     * 例子
         ```shell
-        lftp.exe lget 127.0.0.1::8888 server.txt
-        lftp.exe lsend 127.0.0.1::8888 ../data/client.txt
+        lftp.exe lget 127.0.0.1:8888 server.txt
+        lftp.exe lsend 127.0.0.1:8888 ../data/client.txt
         ```
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,36 +1,43 @@
 #include "client.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     /*
     *   param
     *   op - lget or lsend
     *   server ip - default 127.0.0.1:8888
     *   file - default file dir is ../data
-    */ 
-    try {
+    */
+    try
+    {
         string op = argv[1];
         string serverIp = argv[2];
         string filename = argv[3];
 
         int index = serverIp.find_last_of(':');
-        string host = serverIp.substr(0, index - 1);
+        string host = serverIp.substr(0, index);
         // cout << host << endl;
         int port = atoi(serverIp.substr(index + 1).c_str());
         // cout << port << endl;
 
-        if (op == "lget") {
+        if (op == "lget")
+        {
             Client *client = new Client(host, filename, port);
             client->lget();
         }
-        else if (op == "lsend") {
+        else if (op == "lsend")
+        {
             Client *client = new Client(host, filename, port);
             client->lsend();
         }
-        else {
+        else
+        {
             cout << "Please input as required";
             exit(0);
-        }        
-    } catch (exception &err) {
+        }
+    }
+    catch (exception &err)
+    {
         cout << err.what() << endl;
         cout << "Please input as required";
         exit(1);
